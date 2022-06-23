59ce42c035f46aef7fa9cefe21b2b5e6fd28b0b9
c0untyy <mitko858@gmail.com>
2022-06-23 15:44:05 +0300

    Replace button to create grid with slider
    Replacing annoying button and user text input previously used to
    create grid with a slider - easier and cleaner to use

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ let resetbutton = document.getElementById("reset");
 let color = "black";
 let blackbutton = document.getElementById("black");
 let click = true;
+let slider = document.getElementById("mySlider");
+let defaultsize = 16;
 
+createGrid(defaultsize);
+
+slider.oninput = function () {
+  let currentvalue = slider.value;
+  document.querySelector("b").innerText =
+    currentvalue.toString() + "x" + currentvalue.toString();
+  changeGridSize(currentvalue);
+};
+
+//Before Branch
 function createGrid(size) {
   let squares = grid.querySelectorAll("div");
   squares.forEach((div) => div.remove());
@@ -20,16 +32,9 @@ function createGrid(size) {
   }
 }
 
-function changeGridSize(input) {
-  if (input >= 2 && input <= 100) {
-    document.querySelector(".error").textContent = "";
-    document.querySelector(".error").style.display = "none";
-    createGrid(input);
-  } else {
-    document.querySelector(".error").textContent =
-      "Input must be between 2 and 100";
-    document.querySelector(".error").style.display = "flex";
-  }
+function changeGridSize(currentvalue) {
+  createGrid(currentvalue);
+  console.log(currentvalue);
 }
 
 //Painting
